5d00f86e0d3cb1b538e3ef679fe5210836bb4f79
Vincent Curran <vincentcurran@Vincents-MacBook-Pro-2.local>
2021-09-29 21:13:37 -0600

    added tests for overlap and area

--- test.cpp
+++ test.cpp
@@ -3,9 +3,31 @@
 
 #include "Rectangle.h"
 
-
-TEST_CASE ( "Factorials are computed", "[factorial]") {
-  REQUIRE( Factorial(2) == 2 );
-  REQUIRE( Factorial(3) == 6 );
+TEST_CASE ( "Rectangle constructor", "[Rectangle]") {
+  Point origin = {1,0};
+  Point starter = {0,1};
+  Rectangle square = {origin, starter};
+  REQUIRE( ((square.get_p1().x == origin.x) && (square.get_p1().y == origin.y)));
+  REQUIRE( ((square.get_p2().x == starter.x) && (square.get_p2().y == starter.y)));
 }
 
+TEST_CASE ( "Rectangle overlap", "[Overlaps]") {
+  Point origin = {1,0};
+  Point starter = {0,1};
+  Point starter2 = {0,2};
+  Rectangle square = {origin, starter};
+  Rectangle square2 = {origin, starter};
+  Rectangle square3 = {origin, starter2};
+  CHECK( square.Overlaps(square2));
+  CHECK( square.Overlaps(square3));
+}
+
+TEST_CASE ( "Rectangle area calculated", "[Area]") {
+  Point origin = {1,0};
+  Point starter = {0,1};
+  Point starter2 = {0,4};
+  Rectangle square = {origin, starter};
+  Rectangle square2 = {origin, starter2};
+  CHECK( square.CalculateArea() == 1 );
+  CHECK( square2.CalculateArea() == 4 );
+}
