76ec4ab226a430c7198e2e5f7c37da8b2ebae3c6
Vincent Curran <vincentcurran@Vincents-MacBook-Pro-2.local>
2021-09-30 18:49:06 -0600

    finished test cases

--- test.cpp
+++ test.cpp
@@ -15,11 +15,14 @@ TEST_CASE ( "Rectangle overlap", "[Overlaps]") {
   Point origin = {1,0};
   Point starter = {0,1};
   Point starter2 = {0,2};
+  Point origin2 = {2,0};
   Rectangle square = {origin, starter};
   Rectangle square2 = {origin, starter};
   Rectangle square3 = {origin, starter2};
+  Rectangle square4 = {origin2, starter};
   CHECK( square.Overlaps(square2));
   CHECK( square.Overlaps(square3));
+  CHECK( square.Overlaps(square4));
 }
 
 TEST_CASE ( "Rectangle area calculated", "[Area]") {
@@ -30,4 +33,26 @@ TEST_CASE ( "Rectangle area calculated", "[Area]") {
   Rectangle square2 = {origin, starter2};
   CHECK( square.CalculateArea() == 1 );
   CHECK( square2.CalculateArea() == 4 );
+}
+
+TEST_CASE ( "Rectangle expanded", "[Expand]") {
+  Point origin = {4,2};
+  Point starter = {2,4};
+  Rectangle square = {origin, starter};
+  square.Expand();
+  CHECK(square.get_p1().x == 5);
+  CHECK(square.get_p1().y == 1);
+  CHECK(square.get_p2().x == 1);
+  CHECK(square.get_p2().y == 5);
+}
+
+TEST_CASE ( "Rectangle shrunk", "[Shrink]") {
+  Point origin = {4,2};
+  Point starter = {2,4};
+  Rectangle square = {origin, starter};
+  square.Shrink();
+  CHECK(square.get_p1().x == 3);
+  CHECK(square.get_p1().y == 3);
+  CHECK(square.get_p2().x == 3);
+  CHECK(square.get_p2().y == 3);
 }
